60d48d094eb363735bb9dbecaf3d05333129ed03
Mass <mass.seddaoui@gmail.com>
2024-07-04 04:08:29 +0300

    contitions, some if else statements

--- js-practice.js
+++ js-practice.js
@@ -1,1 +1,11 @@
 let cartQty = 0;
+
+let price = 8;
+
+if (price = 10) {
+  alert ("This price is too pefect to be true, let's not risk it.");
+} else if (price > 10) {
+  console.log ('These guys are the apple of whatever we trying to buy.');
+} else if (price < 10) {
+  alert ("Cheap! suspicious definitly not taking it.");
+}
